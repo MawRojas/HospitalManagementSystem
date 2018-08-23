80eab4c2f13d4ffd2236eea3585189c1d272b393
Satya Vinjamuri <satysai.vinjamuri@gmail.com>
2018-08-23 11:40:01 -0400

    Updated

--- hospital_management_system/Person/views.py
+++ hospital_management_system/Person/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render, get_object_or_404
-from Person.models import Persons, Patient
+from django.shortcuts import render
 
 # Create your views here.
-
-def personList(request):
-	persons=Persons.objects.all()
-def patientList(request):
-	patients=Patient.objects.all()
