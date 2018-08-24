7a083f7b862b4e46fcbc1aea09c9b77d5e234fdc
NoopurParikh <noopurpar@gmail.com>
2018-08-24 12:38:56 -0400

    Payment done

--- hospital_management_system/Payment/views.py
+++ hospital_management_system/Payment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Prescription, Bill
+
 
 # Create your views here.
+
+def prescription_list(request):
+	meds = Prescription.objects.all
+	return render(request, 'payment/prescription_list.html', {'meds':meds})
