3cba6efabe2b9208632a0174a8f8e7d255c7713a
NoopurParikh <noopurpar@gmail.com>
2018-08-24 16:03:22 -0400

    Created App 3 and 4

---
+++ hospital_management_system/Surgery_Details/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Surgery(models.Model):
+	surgery_description = models.TextField()
+	start_time = models.DateTimeField(blank = True,null=True)
+	duration = models.PositiveIntegerField()
+	surgery_price = models.FloatField()
+	# doctor_incharge = models.ForeignKey(Doctor, null=True, related_name = 'surgery', on_delete=models.CASCADE)
+	# nurse_incharge = models.ForeignKey(Nurse, null=True, related_name = 'surgery', on_delete=models.CASCADE)
+	# patient = models.ForeignKey(Patient, null=True, related_name = 'surgery', on_delete=models.CASCADE)
+	# surgery_room = models.ForeignKey(SurgeryRoom, null=True, related_name = 'surgery', on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.name
+
+		
+class Equipment(models.Model):
+	equip_name = models.CharField(max_length=200)
+	equip_description = models.TextField()
+	equip_price = models.FloatField()
