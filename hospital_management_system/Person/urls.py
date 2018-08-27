6c9c94dc3eccb156a927040158691ead8168635d
Satya Vinjamuri <satysai.vinjamuri@gmail.com>
2018-08-27 11:38:45 -0400

    Views

--- hospital_management_system/Person/urls.py
+++ hospital_management_system/Person/urls.py
@@ -6,8 +6,10 @@ from Person.models import Persons, Patient, Employee, Doctors
 urlpatterns = [
 	url(r'^Person', views.addPersons, name='person'),
 	url(r'^Employee', views.addEmployee,name='person'),
-    url(r'^enter_details/patient/(?P<id>\d+)$', views.addPatients, name='patient_details'),
-	url(r'^enter_details/doctor/$', views.addDoctor, name='doctor_details'),
-	url(r'^enter_details/nurse/(?P<id>\d+)$', views.addNurse, name='nurse_details'),	
+	url(r'^patients/$',views.listPatients, name='patient_list'),
+    url(r'^enter_details/patient/$', views.postPatients, name='patient_details'),
+	url(r'^enter_details/doctor/$', views.postDoctors, name='doctor_details'),
+	url(r'^enter_details/nurse/$', views.postNurse, name='nurse_details'),	
+	
 ]
 
--- hospital_management_system/Person/views.py
+++ hospital_management_system/Person/views.py
@@ -1,34 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from Person.models import Persons, Patient, Employee, Doctors
+from hospital.models import Hospital, PatientRoom, SurgeryRoom
+from .forms import addPatient, addNurses, addDoctors, UpdatePatientDetails, UpdateDoctors, UpdateNurses
 # Create your views here.
 
 def addPersons(request):
 	addPerson=Persons.objects.all()
 
-def addPatients(request):
-	instance=get_object_or_404(Patient)
-	form=UpdatePatientDetails(request.POST, instance=instance)
+def listPatients(request):
+	patientList=Person.patientList.all()
+	print(patientList)
+	return render(request, 'listpatients.html', {'patientList':patientList})
+
+
+
+def postPatients(request):
+	hospital=get_object_or_404(Hospital, id=1)
+	form=addPatient(request.POST)
 	if form.is_valid():
 		item=form.save(commit=False)
-		item.is_occupied='1'
 		item.save()
-		return redirect('Person:Patient')
+		hospital.patient_rooms.add(item)
+		return redirect('hospital:rooms')
+	else:
+		form=addPatient()
+		return render(request, 'post_element.html', {'form':form, 'button_title':'Post Patient'})
+
 
 def addEmployee(request):
 	addEmployees=Persons.objects.all()
-def addDoctor(request):	
-	instance=get_object_or_404(Doctors)
-	form=UpdateDoctors(request.POST, instance=instance)
+
+
+
+
+def postDoctors(request):	
+	instance=get_object_or_404(Doctors, id=1)
+	form=addDoctors(request.POST)
 	if form.is_valid():
 		item=form.save(commit=False)
-		item.is_occupied='1'
 		item.save()
-		return redirect('Employee:Doctor')
-def addNurse(request):
-	instance=get_object_or_404(Nurses)
-	form=UpdateNurses(request.POST, instance=instance)
+		hospital.doctors.add(item)
+		return redirect('hospital:rooms')
+	else:
+		form=addDoctors()
+		return render(request, 'post_element.html', {'form':form, 'button_title':'Post Doctors'})
+		
+def postNurse(request):
+	hospital=get_object_or_404(Nurses, id =1)
+	form=addNurses(request.POST)
 	if form.is_valid():
 		item=form.save(commit=False)
-		item.is_occupied='1'
 		item.save()
-		return redirect('Employee:Doctor')
+		hospital.nurses.add(item)
+		return render('Employee:Doctor')
+	else:
+		form=addNurses()
+		return render(request, 'post_element.html', {'form':form, 'button_title':'Post Nurses'})
+
